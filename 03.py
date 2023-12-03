d890f0ecae0c74ec1e123e9946b01a4bb73b83bd
Andrea Waltlova <andywaltlova@gmail.com>
2023-12-03 19:58:55 +0100

    Day 3 part 2
    Signed-off-by: Andrea Waltlova <andywaltlova@gmail.com>

--- 03.py
+++ 03.py
@@ -5,9 +5,9 @@ with open('input.txt') as input:
 def check_direction(lines, row, col):
     try:
         char = lines[row][col]
-        return char != '.' and not char.isdigit()
+        return (char, (row, col)) if char != '.' and not char.isdigit() else None
     except IndexError:
-        return False
+        return None
 
 def part_one(lines):
     num_sum = 0
@@ -44,7 +44,49 @@ def part_one(lines):
 
 
 def part_two(lines):
-    return
+    # Keys coordinates of '*' symbol, value is list of numbers
+    gears_dict = {}
+    for row in range(len(lines)):
+        partial_num_str = ""
+        adjacent_gears = set()
+        for col in range(len(lines[row])):
+            char = lines[row][col]
+            if not char.isdigit():
+                for coord in adjacent_gears:
+                    gears_dict[coord] = gears_dict.get(coord, []) + [int(partial_num_str)]
+                partial_num_str = ""
+                adjacent_gears = set()
+                continue
+            # Is number, need to check for adjacent symbols and filter only for '*'
+            if char.isdigit():
+                partial_num_str += char
+                adjacent_gears |= set(
+                    map(
+                        lambda symbol_coord: symbol_coord[1],
+                        list(
+                            filter(
+                                lambda symbol_coord: symbol_coord is not None and symbol_coord[0] == '*',
+                                [
+                                    check_direction(lines, row, col-1),
+                                    check_direction(lines, row, col+1),
+                                    check_direction(lines, row-1, col),
+                                    check_direction(lines, row+1, col),
+                                    check_direction(lines, row+1, col+1),
+                                    check_direction(lines, row-1, col-1),
+                                    check_direction(lines, row+1, col-1),
+                                    check_direction(lines, row-1, col+1),
+                                ]
+                            )
+                        )
+                    )
+                )
+        # End of line
+        if adjacent_gears and partial_num_str:
+            for coord in adjacent_gears:
+                gears_dict[coord] = gears_dict.get(coord, []) + [int(partial_num_str)]
+            partial_num_str = ""
+
+    return sum(nums[0] * nums[1] for nums in gears_dict.values() if len(nums) == 2)
 
 print(part_one(lines))
-# print(part_two(lines))
+print(part_two(lines))
